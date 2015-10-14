709aa056a9dd588e632d229702771ebcb275297b
Bence Eros <ebence88@gmail.com>
2015-10-14 20:46:08 +0200

    adding handlebars to tsd and bower

--- static-skeleton/js/config.js
+++ static-skeleton/js/config.js
@@ -8,7 +8,8 @@ requirejs.config({
     jquery: "../bower_components/jquery/dist/jquery",
     requirejs: "../bower_components/requirejs/require",
     text: "../bower_components/text/text",
-    "jasmine-core": "../bower_components/jasmine-core/lib/jasmine-core/jasmine"
+    "jasmine-core": "../bower_components/jasmine-core/lib/jasmine-core/jasmine",
+    handlebars: "../bower_components/handlebars/handlebars"
   },
   packages: [
 
